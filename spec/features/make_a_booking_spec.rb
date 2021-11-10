f8c8388dcbc49c7d72522a97b9567ec3f3e8a1d0
NenehB <Neneh.Bojang@digital.justice.gov.uk>
2021-11-10 15:35:07 +0000

    Comments removed

--- spec/features/make_a_booking_spec.rb
+++ spec/features/make_a_booking_spec.rb
@@ -1,13 +1,11 @@
 feature 'Add new booking' do
   scenario 'user can add a new booking' do
     connection = PG.connect(dbname: 'makersbnb_test')
-    # CREATE TABLE booking (id SERIAL PRIMARY KEY, space_id INTEGER, booked_date DATE, resident INTEGER, requested BIT, confirmed BIT);
-
     visit ('/')
-    click_button(:add_space) # This takes us to a new page to fill in
+    click_button(:add_space) 
     fill_in 'booked', with: "2021-12-01"
     fill_in 'to_date', with: "2021-12-31"
-    click_button(:submit) # This takes us back to the listing page
+    click_button(:submit) 
     expect(page).to have_content "Monsters Inc"
 
   end
--- spec/unit/booking_spec.rb
+++ spec/unit/booking_spec.rb
@@ -6,10 +6,6 @@ describe 'Booking' do
   it 'Can add a new booking' do
     setup_test_database
     Booking.add(space_id: "1", booked_date: "2021-12-12", resident: "20", requested: "1", confirmed: "0" )
-
-    p '+++++++++'
-    p Booking.all
-    p '+++++++++'
     expect(Booking.all.first.booked_date).to include "2021-12-12"
   end
 
