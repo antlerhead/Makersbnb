3333d770727ac697f137decd48f0b612169ad24b
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2021-11-11 15:42:56 +0000

    updated add_space_spec file

--- spec/features/add_space_spec.rb
+++ spec/features/add_space_spec.rb
@@ -5,13 +5,13 @@ feature 'Add new spaces' do
     connection.exec("INSERT INTO spaces VALUES(1,'Blossom Villa', 'Pretty villa', 150.00, '2021-10-01', '2021-10-07', 10 , 1 );")
     
     visit ('/')
-    click_button(:add_space) # This takes us to a new page to fill in
+    click_link 'Add Space' # This takes us to a new page to fill in
     fill_in 'name', with: "Monsters Inc"
     fill_in 'description', with: "Faulous University residence"
     fill_in 'price', with: "120"
     fill_in 'from_date', with: "2021-12-01"
     fill_in 'to_date', with: "2021-12-31"
-    click_button(:submit) # This takes us back to the listing page
+    click_button 'add_space' # This takes us back to the listing page
     expect(page).to have_content "Monsters Inc"
 
   end
