87116fb2fd493d8a1265fedc29d82cb8be131cac
Max <36797697+antlerhead@users.noreply.github.com>
2021-11-26 22:35:58 +0000

    Update make_a_booking_spec.rb
    removed the extra visit!

--- spec/features/make_a_booking_spec.rb
+++ spec/features/make_a_booking_spec.rb
@@ -3,7 +3,6 @@ feature 'Add new booking' do
     connection = PG.connect(dbname: 'makersbnb_test')
     visit ('/')
     find('button').click
-    visit ('/booking_form')
     fill_in('date_pick', with: "2021-11-24")
     click_button
     expect(page).to have_content "2021-11-24"
